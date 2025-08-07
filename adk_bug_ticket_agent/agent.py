f96e633db623d40bdb7d1ce2183e2b944aa9ebd2
yannipeng <yannipeng@google.com>
2025-08-07 08:39:36 -0700

    add comment

--- adk_bug_ticket_agent/agent.py
+++ adk_bug_ticket_agent/agent.py
@@ -3,6 +3,7 @@ from . import system_prompt
 from .tools.tools import get_current_date, search_tool, toolbox_tools
 from google.adk.tools import load_memory
 
+# this is only used by adk web not in the django framework.
 def get_root_agent():
     print("Root agent initialized.")
     return Agent(
