e4c921596c8b2f09393cf50565758cd9acb067c0
yannipeng <yannipeng@google.com>
2025-08-07 08:34:16 -0700

    update dockeringore and add agent.py

---
+++ adk_bug_ticket_agent/agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents import Agent
+from . import system_prompt
+from .tools.tools import get_current_date, search_tool, toolbox_tools
+from google.adk.tools import load_memory
+
+def get_root_agent():
+    print("Root agent initialized.")
+    return Agent(
+            model="gemini-2.5-flash",
+            name="it_bug_assistant_agent",
+            instruction=system_prompt.agent_instruction,
+            tools=[load_memory, get_current_date, search_tool, *toolbox_tools],
+    )
+
+
+root_agent = get_root_agent()
