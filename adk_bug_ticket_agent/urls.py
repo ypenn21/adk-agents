095894931be35ed11ed8b5be432c3052142b3db1
yannipeng <yannipeng@google.com>
2026-09-12 15:34:17 -0500

    maped root of uri

--- adk_bug_ticket_agent/urls.py
+++ adk_bug_ticket_agent/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('interact/', views.interact_with_agent, name='interact_with_agent'),
+    path("", views.interact_with_agent, name="interact_root"),
+    path("interact/", views.interact_with_agent, name="interact_with_agent"),
+    path("chat/", views.interact_with_agent, name="chat"),
 ]
--- web/urls.py
+++ web/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from adk_bug_ticket_agent import views as agent_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("agent/", include("adk_bug_ticket_agent.urls"))
+    path("", agent_views.interact_with_agent, name="root"),
+    path("agent/", include("adk_bug_ticket_agent.urls")),
 ]
